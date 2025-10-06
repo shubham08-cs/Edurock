4fef328d2f217c7240ed6a8107e456b09e68be7f
Shubham Singh <me.shubhamsingh8998@gmail.com>
2025-10-06 19:32:30 +0530

    Add files via upload

---
+++ services/session.php
@@ -0,0 +1,28 @@
+<?php
+// Initialize the session
+
+require_once __DIR__ . '/database.php';
+
+
+session_start();
+
+function checkAuth($email)
+{
+    if (DatabaseService::getInstance()->checkUser($email)) {
+        setSesstion();
+        return true;
+    } else {
+        return "Email is not valid";
+    }
+}
+
+function setSesstion()
+{
+    $_SESSION["user"] = true;
+}
+
+function logoutSesstion()
+{
+    $_SESSION["user"] = false;
+}
+
