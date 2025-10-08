9794effcda6f552396fe1331761621b6b6525d6a
Anamika Vyavahare <aanunm2005@gmail.com>
2025-10-08 16:34:28 +0530

    Removed dark mode toggle and set site to light mode only

--- home-5.php
+++ home-5.php
@@ -47,18 +47,7 @@
     </div>
     <!-- pre loader area end -->
 
-    <!-- Dark/Light area start -->
-    <div class="mode_switcher my_switcher">
-        <button id="light--to-dark-button" class="light align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon dark__mode" viewBox="0 0 512 512"><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
-
-            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon light__mode" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"/><circle cx="256" cy="256" r="80" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32"/></svg>
-
-            <span class="light__mode">Light</span>
-            <span class="dark__mode">Dark</span>
-        </button>
-    </div>
-    <!-- Dark/Light area end -->
+    
 
     <main class="main_wrapper overflow-hidden">
 
@@ -2537,7 +2526,7 @@
         <!-- register__section__start-->
 
         <!-- team__section__start -->
-        <div class="teamarea sp_bottom_100 sp_top_50">
+        <!-- <div class="teamarea sp_bottom_100 sp_top_50">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-12" data-aos="fade-up">
@@ -2639,12 +2628,12 @@
 
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <!-- team__section__end-->
 
         <!-- testimonial__section__start -->
-        <div class="testimonialarea__3">
+        <!-- <div class="testimonialarea__3">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
@@ -2713,7 +2702,7 @@
                 </div>
             </div>
 
-        </div>
+        </div> -->
         <!-- testimonial__section__start -->
 
         <!-- blog__section__start -->
