f348a0f6465bb59ef1ff5d757467215bf9d71fa5
ParthBargale <parthbargale16@gmail.com>
2025-10-14 11:19:10 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,10 +20,18 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('header');
 		$this->load->view('home');
+		
+		$this->load->view('footer');
+
+
 	}
 	public function about()
 	{
 		$this->load->view('about');
+		
+
 	}
+
 }
